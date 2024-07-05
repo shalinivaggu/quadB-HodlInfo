2439374267b6569a19b111c0999045f15d56866b
shalinivaggu <shalinivaggu@gmail.com>
2024-07-05 16:17:34 +0530

    Add periodic data fetch and display
    - Implemented functionality to fetch new  data every 1 minute
    - Updated script.js to display the fetched data changes
    - Ensured the script reboots the fetching process on each data update

--- public/script.js
+++ public/script.js
@@ -5,20 +5,19 @@ const baseId = document.getElementById("baseId")
 const spinner = document.getElementById("spinner")
 const togglecheck = document.getElementById("togglecheck")
 const theme = document.getElementById("theme")
+const tickerTableBody = document.getElementById('dataTableBody');
 
 let countdown = 60;
-let interval = setInterval(() => {
-  countdown--;
-  countdownText.textContent = countdown;
-  if (countdown === 0) {
-    countdown = 60;
-    countdownText.textContent = countdown;
-  }
-}, 1000);
+let obj  = {value: selectBase.value};
 
-let average = 0 ;
-// Fetch ticker data
-  fetch(`/tickers/`)
+
+function fetchData(obj){
+  // Fetch ticker data
+  const {value} = obj;
+  let average = 0 ;
+  spinner.classList.remove('d-none');
+  tickerTableBody.innerHTML = "";
+  fetch(`/tickers/${value}/`)
     .then(response => response.json())
     .then(data => {
       spinner.classList.add('d-none')
@@ -43,43 +42,29 @@ let average = 0 ;
     .catch(error => {
       console.error('Error fetching ticker data:', error);
     });
+}
 
-    selectBase.addEventListener("change" , function(event){
-      const tickerTableBody = document.getElementById('dataTableBody');
-      tickerTableBody.innerHTML = "";
-      const base = event.target.value;
-      baseId.textContent = base;
-      spinner.classList.remove('d-none')
-      fetch(`/tickers/${base}/`)
-      .then(response => response.json())
-      .then(data => {
-        spinner.classList.add('d-none')
-        let average = 0;
-        const tickerData = data;
-        console.log(tickerData);
-        tickerData.forEach((ticker, index) => {
-          const row = tickerTableBody.insertRow();
-          row.classList.add("tablerow");
-          row.insertCell().textContent = index + 1;
-          row.insertCell().textContent = ticker.name;
-          row.insertCell().textContent = ticker.last;
-          row.insertCell().textContent = `$${ticker.buy} / $${ticker.sell}`;
-          row.insertCell().textContent = `${parseFloat(ticker.volume).toFixed(2)}%`;
-          row.insertCell().textContent = ticker.savings;
-          average += parseInt(ticker.last);
-        });
-        avgvalue.textContent = `${parseInt(average / tickerData.length)}`;
-        if (tickerData.length > 0 && average > 0)
-          avgvalue.textContent = `${parseInt(average / tickerData.length)}`;
-        else 
-          avgvalue.textContent = '0';
-      })
-      .catch(error => {
-        console.error('Error fetching ticker data:', error);
-      });
-  
-    })
+let interval = setInterval(() => {
+  countdown--;
+  countdownText.textContent = countdown;
+  if (countdown === 0) {
+    countdown = 60;
+    fetchData(obj);
+    countdownText.textContent = countdown;
+  }
+}, 1000);
+
+fetchData(obj);
+
+selectBase.addEventListener("change" , function(event){
+    const base = event.target.value;
+    baseId.textContent = base;
+    obj.value = base.toLowerCase();
+    console.log(base);
+    fetchData(obj);
+})
 
 togglecheck.addEventListener("change" , function(event) {
   theme.classList.toggle("theme-dark");
 })
+
